d59bb4954fb582067ce9004641ab80c8fa5828f3
ayaph <ayaph@champ.aero>
2019-10-11 19:57:53 +0800

    For compile

--- src/Chapter8/ConsoleReadInputSample.java
+++ src/Chapter8/ConsoleReadInputSample.java
@@ -2,10 +2,24 @@ package Chapter8;
 
 import java.io.BufferedReader;
 import java.io.Console;
+import java.io.IOException;
 
+
+/**
+ * DOCUMENT ME!
+ *
+ * @version  $Revision$, $Date$
+ */
 public class ConsoleReadInputSample
 {
-	public static void main(String[] args)
+	//~ Methods ----------------------------------
+	/**
+	 * DOCUMENT ME!
+	 *
+	 * @param   args
+	 * @throws  IOException
+	 */
+	public static void main(String[] args) throws IOException
 	{
 		Console console = System.console();
 		if (console == null)
@@ -17,17 +31,17 @@ public class ConsoleReadInputSample
 			console.writer().print("Excitement Level: ");
 			console.flush();
 			String excitementLevel = console.readLine();
-			
+
 			String name = console.readLine("What is your name? ");
-			
+
 			Integer age = null;
 			console.writer().print("Age: ");
 			console.flush();
-			
+
 			BufferedReader reader = new BufferedReader(console.reader());
 			String value = reader.readLine();
 			age = Integer.parseInt(value);
-			
+
 			console.writer().println();
 			console.format("Your name is: " + name);
 			console.writer().println();
